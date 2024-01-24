01529d89386aefe25781897d07218b5f2c9105c9
robonexx <robertwagar@gmail.com>
2024-01-24 20:36:48 +0100

    ...

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,5 +1,5 @@
 /* import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
 interface User {
