928c0adc69477ae48174886420ed766877641199
robonexx <robertwagar@gmail.com>
2024-01-24 08:55:23 +0100

    will change image component2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
+import Logo from "./_components/Logo";
 
 const Home = async () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Lading page första sidan</h1>
-      <Link href='/auth/login'>Login</Link>
-      <Link href='/auth/register'>Register</Link>
+      {/* <Link href='/auth/login'>Login</Link>
+      <Link href='/auth/register'>Register</Link> */}
+      <Logo
+        
+      />
     </main>
   );
 }
