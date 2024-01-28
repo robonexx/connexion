7ff3ce3b6ab8044139be7e01ecf72ea0f81a0f47
robonexx <robertwagar@gmail.com>
2024-01-28 07:35:28 +0100

    ...

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,7 +14,8 @@ const mockData: PostItemProps[] = [
     _id: '1',
     author: { _id: '4749879853', name: 'John Doe' },
     createdAt: '2022-01-01',
-    image: 'https://images.pexels.com/photos/4637225/pexels-photo-4637225.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    image:
+      'https://images.pexels.com/photos/4637225/pexels-photo-4637225.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
     title: 'First Post',
     body: 'This is the body of the first post.',
     link: 'https://link1.com',
@@ -24,17 +25,19 @@ const mockData: PostItemProps[] = [
     _id: '2',
     author: { _id: '47498798d53', name: 'Jill Smith' },
     createdAt: '2022-02-15',
-    image: 'https://images.pexels.com/photos/12437649/pexels-photo-12437649.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    image:
+      'https://images.pexels.com/photos/12437649/pexels-photo-12437649.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
     title: 'Second Post',
     body: 'This is the body of the second post.',
-    link:'https://link3.com',
+    link: 'https://link3.com',
     category: 'Schema ändring',
   },
   {
     _id: '3',
     author: { _id: '5549879853', name: 'Bob Doe' },
     createdAt: '2022-03-30',
-    image: 'https://images.pexels.com/photos/16307204/pexels-photo-16307204/free-photo-of-ljus-svartvitt-stad-man.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    image:
+      'https://images.pexels.com/photos/16307204/pexels-photo-16307204/free-photo-of-ljus-svartvitt-stad-man.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
     title: 'Third Post',
     body: 'This is the body of the third post.',
     link: 'https://link4.com',
@@ -51,38 +54,41 @@ type SearchTypes = {
 
 const Dashboard = async ({ searchParams }: SearchTypes) => {
   // fetch the data
- /*  const { data: session } = useSession(); */
+  /*  const { data: session } = useSession(); */
   const q = searchParams?.q || '';
   const page = searchParams?.page || '1';
 
   const session = await getServerSession(authOptions);
-  console.log('session from dashboard: ', session)
+  console.log('session from dashboard: ', session);
 
   const { count, posts } = await fetchPosts(q, page);
 
-
-/*   if (session?.user.role !== "admin") {
+  /*   if (session?.user.role !== "admin") {
       return <h1 className="text-5xl">Access Denied</h1>
  }
  */
   return (
     <div className='relative flex min-h-screen h-full w-full flex-col items-center red-gradient py-20 px-20'>
-      <h1 className='text-4xl tracking-wide font-semibold capitalize text-[#ff4040]'>Senaste inlägg!</h1>
+      <h1 className='text-4xl tracking-wide font-semibold capitalize text-[#ff4040]'>
+        Senaste inlägg!
+      </h1>
       <div className='min-h-screen pt-8 pb-8'>
         <div className='w-fit max-w-4xl grid place-items-center mx-auto bg-transparent'>
           <section className='w-full h-full flex flex-col md:flex-row md:flex-wrap gap-5'>
-          {posts.map(({ _id, createdAt, title, tags, body, image, author }) => (
-          <Post
-            key={_id}
-            title={title}
-            createdAt={createdAt}
-            body={body}
-            tags={tags}
-            _id={_id}
-            image={image}
-            author={author}
-          />
-        ))}
+            {posts.map(
+              ({ _id, createdAt, title, tags, body, image, author }) => (
+                <Post
+                  key={_id}
+                  title={title}
+                  createdAt={createdAt}
+                  body={body}
+                  tags={tags}
+                  _id={_id}
+                  image={image}
+                  author={author}
+                />
+              )
+            )}
           </section>
         </div>
         <Pagination count={count} />
--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
-import { CldImage } from "next-cloudinary";
-import DeleteButton from "../delete-button/DeleteButton";
-import { getServerSession } from "next-auth/next";
-import ImageContainer from "../image-container/ImageContainer";
+import Image from 'next/image';
+import Link from 'next/link';
+import { CldImage } from 'next-cloudinary';
+import DeleteButton from '../delete-button/DeleteButton';
+import { getServerSession } from 'next-auth/next';
+import ImageContainer from '../image-container/ImageContainer';
 /* import { authOptions } from "@/app/api/auth/[...nextauth]/route"; */
-import { PostItemProps } from "@/types/Types";
-import { convertDate } from "@/utils/convertDate";
-import { timeAgo } from "@/utils/timeAgo";
-import CustomLink from "../custom-link/CustomLink";
-import { VscEye } from "react-icons/vsc";
+import { PostItemProps } from '@/types/Types';
+import { convertDate } from '@/utils/convertDate';
+import { timeAgo } from '@/utils/timeAgo';
+import CustomLink from '../custom-link/CustomLink';
+import { VscEye } from 'react-icons/vsc';
 
 const Post = ({
   _id,
@@ -21,16 +21,16 @@ const Post = ({
   author,
   category,
 }: PostItemProps) => {
- /*  const session = await getServerSession(); */
+  /*  const session = await getServerSession(); */
 
-    /* const isEditable = session && session?.user?.id === author.id; */
+  /* const isEditable = session && session?.user?.id === author.id; */
 
   return (
-    <div className="relative my-4 border-b border-[#888] py-8 max-w-96">
-      <div className="mb-4">
+    <div className='relative my-4 border-b border-[#888] py-8 max-w-96'>
+      <div className='mb-4'>
         {author ? (
           <>
-            Posted by: <span className="font-bold">{author.name}</span> on{" "}
+            Posted by: <span className='font-bold'>{author.name}</span> on{' '}
             {convertDate(createdAt)}
           </>
         ) : (
@@ -38,24 +38,22 @@ const Post = ({
         )}
       </div>
 
-      <div className="w-full h-72 relative">
+      <div className='w-full h-72 relative'>
         {image ? (
-          <ImageContainer
-            imageData={image}
-          />
+          <ImageContainer imageData={image} />
         ) : (
           <Image
-            src={"/thumbnail-placeholder.png"}
+            src={'/thumbnail-placeholder.png'}
             alt={title}
             fill
-            className="object-cover rounded-md object-center"
+            className='object-cover rounded-md object-center'
           />
         )}
       </div>
 
       {category && (
         <Link
-          className="bg-[#ff4040] w-fit text-white px-4 py-0.5 text-sm font-bold rounded-md mt-4 block"
+          className='bg-[#ff4040] w-fit text-white px-4 py-0.5 text-sm font-bold rounded-md mt-4 block'
           href={`categories/${category}`}
         >
           {category}
@@ -63,37 +61,37 @@ const Post = ({
       )}
 
       <h2>{title}</h2>
-      <p className="content">{body}</p>
-            <div className="flex gap-2 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                />
-              </svg>
-
-              <Link className="link" href={`${link}`}>
-                {link}
-              </Link>
-      </div>
-      
-      <Link
-          href={`/dashboard/posts/${_id}`}
-          title=''
-          className='text-white text-xl w-fit absolute right-2 bottom-2 top-auto flex items-center'
+      <p className='content'>{body}</p>
+      <div className='flex gap-2 items-center'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
         >
-        <span className='text-xs font-mono mx-1'>Read more</span> <VscEye />
-        </Link>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244'
+          />
+        </svg>
 
-     {/*  {isEditable && (
+        <Link className='link' href={`${link}`}>
+          {link}
+        </Link>
+      </div>
+
+      <Link
+        href={`/dashboard/posts/${_id}`}
+        title=''
+        className='text-white text-xl w-fit absolute right-2 bottom-2 top-auto flex items-center'
+      >
+        <span className='text-xs font-mono mx-1'>Read more</span> <VscEye />
+      </Link>
+
+      {/*  {isEditable && (
         <div className="flex gap-3 font-bold py-2 px-4 rounded-md bg-slate-200 w-fit">
           <Link href={`/edit-post/${id}`}>Edit</Link>
           <DeleteButton id={id} />
@@ -101,6 +99,6 @@ const Post = ({
       )} */}
     </div>
   );
-}
+};
 
-export default Post
+export default Post;
